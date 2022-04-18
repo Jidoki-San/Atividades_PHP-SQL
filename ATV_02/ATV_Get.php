415168e23ebcd553f58e021b0411b94a3a8e8808
Jidoki-San <guiferreira8545@gmail.com>
2022-04-18 01:32:50 -0300

    .

---
+++ ATV_02/ATV_Get.php
@@ -0,0 +1,20 @@
+<?php
+
+    $nome = $_GET['nome'];
+    $senha = $_GET['senha'];
+    $email = $_GET['email'];
+?>
+
+<html>
+    <head>
+        <title>Resposta - GET</title>
+        <meta charset="utf-8">
+        <style></style>
+    </head>
+
+    <body>
+        <div></div>
+    </body>
+
+</html>
+            
---
+++ ATV_02/ATV_Post.php
@@ -0,0 +1,22 @@
+<?php
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $telefone = $_POST['telefone'];
+?>
+
+<html>
+    <head>
+        <title>Respsta - POST</title>
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <p>Nome</p>
+        <p>Email</p>
+        <p>Senha</p>
+        <p>Telefone</p>
+    </body>
+
+</html>
