5cf15b388c49d07e7eb0420b0edab4cc2ac8f815
Jidoki-San <guiferreira8545@gmail.com>
2022-06-16 01:22:52 -0300

    opa

--- ATV_18/php/profile.php
+++ ATV_18/php/profile.php
@@ -7,17 +7,18 @@
     <head>
         <title>Perfil</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href=".../css/">
+        <link rel="stylesheet" href="../css/st_perfil.css">
     </head>
 
     <body>
+        
         <div class="perfil_box">
             <div class="title_box">
                 Informações do usuário
             </div>
-            
+
             <div class="img_box">
-                <img src="<?php echo $row['foto'] ?>">
+                <img src=" <?php echo $row['foto'] ?> ">
             </div>
 
             <div class="info_box">
@@ -25,7 +26,20 @@
                     <label>Usuário</label>
                     <?php echo $row['login'] ?>
                 </span>
+                
+                <span>
+                    <label>Nome</label>
+                    <?php echo $row['nome'] ?>
+                </span>
+                
+                <span>
+                    <label>Sobrenome</label>
+                    <?php echo $row['sobrenome'] ?>
+                </span>
+
+                <a href="painel.php">Voltar</a>
             </div>
+
         </div>
 
     </body>
